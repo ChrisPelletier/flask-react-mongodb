22fd45fca47c2ca34eaf2b72bf3bbf90805d7893
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-09-21 13:38:32 -0700

    Trying to add webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require("webpack");
+
+module.exports = {
+    entry: __dirname + '/static/js/App.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets:['react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin({})
+    ]
+};
