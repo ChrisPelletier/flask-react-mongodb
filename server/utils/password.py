66bfeff478b6663f59220edc3f5abf91ddee41f3
Christopher Pelletier <chris.r.pelletier@gmail.com>
2017-09-21 17:13:28 -0700

    Don’t need these

--- server/utils/password.py
+++ server/utils/password.py
@@ -4,6 +4,4 @@ def encrypt_password(password):
     return bcrypt.hashpw(password.encode('ascii'), bcrypt.gensalt(14))
 
 def verify_password(password_hash, password):
-    print(password_hash)
-    print(password)
     return bcrypt.checkpw(password.encode('ascii'), password_hash)
